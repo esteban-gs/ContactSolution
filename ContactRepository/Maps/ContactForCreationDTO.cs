ea3625ad1a8bcc74634d486db0b12c03f41852d7
esteban-gs <comsat61@gmail.com>
2020-05-07 02:07:12 -0700

    Added AutoMapper, AutoMapperFactory, AutoMapperProfiles, DTOs

---
+++ ContactRepository/Maps/ContactForCreationDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactRepository.Maps
+{
+    class ContactForCreationDTO
+    {
+    }
+}
---
+++ ContactRepository/Maps/MapperFactory.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ContactDB;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace ContactRepository.Maps
+{
+    public static class MapperFactory<TSource, TDest> 
+        where TSource : class
+        where TDest : class
+    {
+        public static IMapper Mapper 
+        { 
+            get
+            {
+                return new Mapper(CreateIt());
+            }
+        }
+
+        public static MapperConfiguration CreateIt()
+        {
+            return new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDest>());
+        }
+    }
+}
